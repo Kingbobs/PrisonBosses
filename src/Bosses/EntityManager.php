641a1e7576d5edefcbd98884e17f4c3bf6fa36c8
esh123cookie <52058925+esh123cookie@users.noreply.github.com>
2020-03-21 18:23:19 -0400

    Update EntityManager.php

--- src/Bosses/EntityManager.php
+++ src/Bosses/EntityManager.php
@@ -6,7 +6,6 @@ namespace Bosses;
 
 use Bosses\Entities\ForgottenKing;
 use Bosses\Entities\ForgottenNome;
-use Bosses\Entities\ForgottenSkeleton;
 use pocketmine\entity\Entity;
 
 class EntityManager extends Entity
@@ -16,6 +15,5 @@ class EntityManager extends Entity
     {
         self::registerEntity(ForgottenKing::class, true, ["king"]);
         self::registerEntity(ForgottenNome::class, true, ["nome"]);
-        self::registerEntity(ForgottenSkeleton::class, true, ["skeleton"]);
     }
 }
